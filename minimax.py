e660396a9b4ec53ac51a7820dae3c9e1fb5963ea
Alan Wagner <alanwagner333@gmail.com>
2016-11-29 21:49:24 -0500

    implementing alphabeta search

--- minimax.py
+++ minimax.py
@@ -1,9 +1,17 @@
+import contextlib
 import copy
 import dominoes
 import operator
 import time
 
 FIXED_MOVES = 5
+
+@contextlib.contextmanager
+def timer(action):
+    start = time.time()
+    yield
+    elapsed = time.time() - start
+    print(action, 'took', int(elapsed), 'seconds')
 
 def make_moves(game):
     for move in game.valid_moves[:-1]:
@@ -35,6 +43,27 @@ def minimax(game):
             best_moves.insert(0, move)
     return best_moves, best_value
 
+def alphabeta(game, alpha=-float('inf'), beta=float('inf')):
+    if game.result is not None:
+        return pow(-1, game.result.player) * game.result.points
+
+    if game.turn % 2:
+        value = float('inf')
+        for _, new_game in make_moves(game):
+            value = min(value, alphabeta(new_game, alpha, beta))
+            beta = min(beta, value)
+            if beta <= alpha:
+                break
+        return value
+    else:
+        value = -float('inf')
+        for _, new_game in make_moves(game):
+            value = max(value, alphabeta(new_game, alpha, beta))
+            alpha = max(alpha, value)
+            if beta <= alpha:
+                break
+        return value
+
 # initializing random game
 game = dominoes.Game.new()
 
@@ -42,20 +71,14 @@ game = dominoes.Game.new()
 for _ in range(FIXED_MOVES):
     game.make_move(*game.valid_moves[0])
 
-# copy the game before computing optimal play
-orig_game = copy.deepcopy(game)
+# run minimax
+with timer('minimax'):
+    game_copy = copy.deepcopy(game)
+    game_copy.skinny_board()
+    print(minimax(game_copy))
 
-# changing to skinny board representation
-game.skinny_board()
-
-# compute optimal play
-start = time.time()
-best_moves, _ = minimax(game)
-elapsed = time.time() - start
-print('Computing optimal play took {} seconds.'.format(int(elapsed)))
-
-# print optimal play
-print(orig_game)
-for move in best_moves:
-    orig_game.make_move(*move)
-    print(orig_game)
+# run alphabeta
+with timer('alphabeta'):
+    game_copy = copy.deepcopy(game)
+    game_copy.skinny_board()
+    print(alphabeta(game_copy))
