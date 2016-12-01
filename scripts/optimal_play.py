7f14939010dffa15e68384a23658342c05f4597f
Alan Wagner <alanwagner333@gmail.com>
2016-11-30 23:57:25 -0500

    using lib in optimal_play.py

--- scripts/optimal_play.py
+++ scripts/optimal_play.py
@@ -1,70 +1,8 @@
-import contextlib
 import copy
 import dominoes
-import operator
-import time
+import lib
 
 FIXED_MOVES = 6
-
-@contextlib.contextmanager
-def timer(action):
-    start = time.time()
-    yield
-    elapsed = time.time() - start
-    print(action, 'took', elapsed, 'seconds')
-
-def make_moves(game):
-    for move in game.valid_moves[:-1]:
-        new_game = copy.deepcopy(game)
-        new_game.make_move(*move)
-        yield move, new_game
-
-    move = game.valid_moves[-1]
-    game.make_move(*move)
-    yield move, game
-
-def minimax(game):
-    if game.result is not None:
-        return [], pow(-1, game.result.player) * game.result.points
-
-    if game.turn % 2:
-        best_value = float('inf')
-        op = operator.lt
-    else:
-        best_value = -float('inf')
-        op = operator.gt
-
-    for move, new_game in make_moves(game):
-        moves, value = minimax(new_game)
-        if op(value, best_value):
-            best_value = value
-            best_moves = moves
-            best_moves.insert(0, move)
-    return best_moves, best_value
-
-def alphabeta(game, alpha_beta=(-float('inf'), float('inf'))):
-    if game.result is not None:
-        return [], pow(-1, game.result.player) * game.result.points
-
-    if game.turn % 2:
-        best_value = float('inf')
-        op = operator.lt
-        update = lambda ab, v: (ab[0], min(ab[1], v))
-    else:
-        best_value = -float('inf')
-        op = operator.gt
-        update = lambda ab, v: (max(ab[0], v), ab[1])
-
-    for move, new_game in make_moves(game):
-        moves, value = alphabeta(new_game, alpha_beta)
-        if op(value, best_value):
-            best_value = value
-            best_moves = moves
-            best_moves.insert(0, move)
-            alpha_beta = update(alpha_beta, best_value)
-            if alpha_beta[1] <= alpha_beta[0]:
-                break
-    return best_moves, best_value
 
 # initializing random game
 game = dominoes.Game.new()
@@ -73,14 +11,15 @@ game = dominoes.Game.new()
 for _ in range(FIXED_MOVES):
     game.make_move(*game.valid_moves[0])
 
-# run minimax
-with timer('minimax'):
+def run(game, function):
     game_copy = copy.deepcopy(game)
     game_copy.skinny_board()
-    print(minimax(game_copy))
+    print(function(game_copy))
+
+# run minimax
+with lib.utils.timer('minimax'):
+    run(game, lib.search.minimax)
 
 # run alphabeta
-with timer('alphabeta'):
-    game_copy = copy.deepcopy(game)
-    game_copy.skinny_board()
-    print(alphabeta(game_copy))
+with lib.utils.timer('alphabeta'):
+    run(game, lib.search.alphabeta)
