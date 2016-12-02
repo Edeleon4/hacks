93c9f4f56848938606031ea4b66f38e3a2c4b23e
Alan Wagner <alanwagner333@gmail.com>
2016-12-02 00:05:36 -0500

    implementing parallel minimax

--- lib/search.py
+++ lib/search.py
@@ -1,4 +1,5 @@
 import copy
+import multiprocessing
 import operator
 
 def make_moves(game):
@@ -29,6 +30,60 @@ def minimax(game):
             best_moves = moves
             best_moves.insert(0, move)
     return best_moves, best_value
+
+class Node:
+    def __init__(self, game, moves):
+        self.game = game
+        self.turn = game.turn
+        self.parent_moves = moves
+
+def node_bfs(node, depth):
+    nodes = [node]
+    for _ in range(depth):
+        new_nodes = []
+        for node in nodes:
+            node.children = []
+            for move, game in make_moves(node.game):
+                child = Node(game, node.parent_moves + [move])
+                node.children.append(child)
+                new_nodes.append(child)
+            node.game = None
+
+        nodes = new_nodes
+
+    return nodes
+
+def node_minimax(node):
+    try:
+        return node.parent_moves + node.child_moves, node.value
+    except AttributeError:
+        pass
+
+    if node.turn % 2:
+        op = min
+    else:
+        op = max
+
+    return op(
+        (node_minimax(child) for child in node.children),
+        key=lambda moves_value: moves_value[1]
+    )
+
+def parallel_minimax(game, serial_depth, num_processes=None, chunk_size=1):
+    # first, BFS from the root game to a certain depth
+    root = Node(game, [])
+    nodes = node_bfs(root, serial_depth)
+
+    # then, run minimax, in parallel, on all the games in the BFS frontier
+    with multiprocessing.Pool(num_processes) as pool:
+        imap = pool.imap(minimax, (n.game for n in nodes), chunk_size)
+        for node, (best_moves, best_value) in zip(nodes, imap):
+            node.game = None
+            node.child_moves = best_moves
+            node.value = best_value
+
+    # finally, run serial minimax from the root to the BFS frontier
+    return node_minimax(root)
 
 def alphabeta(game, alpha_beta=(-float('inf'), float('inf'))):
     if game.result is not None:
--- scripts/optimal_play.py
+++ scripts/optimal_play.py
@@ -2,9 +2,11 @@ import copy
 import dominoes
 import lib
 
-FIXED_MOVES = 5
+FIXED_MOVES = 6
+SERIAL_DEPTH = 4
 
 print('FIXED_MOVES:', FIXED_MOVES)
+print('SERIAL_DEPTH:', SERIAL_DEPTH)
 
 # initializing random game
 game = dominoes.Game.new()
@@ -18,10 +20,21 @@ game.skinny_board()
 
 # run minimax
 with lib.utils.timer() as t_minimax:
-    _, score = lib.search.minimax(copy.deepcopy(game))
-print('minimax took', t_minimax.elapsed, 'seconds and produced a score of', score)
+    moves, score = lib.search.minimax(copy.deepcopy(game))
+print('minimax took', t_minimax.elapsed, 'seconds and produced a score of', score, 'with moves:')
+print(moves)
+
+# run parallel minimax
+with lib.utils.timer() as t_parallel_minimax:
+    moves, score = lib.search.parallel_minimax(copy.deepcopy(game), SERIAL_DEPTH)
+print('parallel minimax took', t_parallel_minimax.elapsed, 'seconds and produced a score of', score, 'with moves:')
+print(moves)
 
 # run alphabeta
 with lib.utils.timer() as t_alphabeta:
-    _, score = lib.search.alphabeta(copy.deepcopy(game))
-print('alphabeta took', t_alphabeta.elapsed, 'seconds and produced a score of', score)
+    moves, score = lib.search.alphabeta(copy.deepcopy(game))
+print('alphabeta took', t_alphabeta.elapsed, 'seconds and produced a score of', score, 'with moves:')
+print(moves)
+
+print('parallel minimax / minimax:', t_parallel_minimax.elapsed / t_minimax.elapsed)
+print('alphabeta / parallel minimax:', t_alphabeta.elapsed / t_parallel_minimax.elapsed)
