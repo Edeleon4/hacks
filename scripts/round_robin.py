1ded23487ffa73db5169326dc3236e7d4bc37b9b
Alan Wagner <alanwagner333@gmail.com>
2016-12-04 17:14:31 -0500

    packaging play() into a function

--- scripts/round_robin.py
+++ scripts/round_robin.py
@@ -3,44 +3,59 @@ import lib.players
 
 STARTS_PER_PLAYER = 250
 
-players = [
-    lib.players.random,
-    lib.players.random,
+team0 = (
     lib.players.random,
     lib.players.random
-]
+)
+team1 = (
+    lib.players.random,
+    lib.players.random
+)
 
 # print config info
 print('STARTS_PER_PLAYER:', STARTS_PER_PLAYER)
-for p in range(len(players)):
-    print('Player {}:'.format(p), players[p].__name__)
 
-# to keep track of how many times each team wins
-wins = [0, 0]
+def play(team0, team1):
+    # set playing order
+    players = (
+        team0[0],
+        team1[0],
+        team0[1],
+        team1[1]
+    )
 
-# to keep track of how many points each team scores
-points = [0, 0]
+    # print config info
+    for p in range(len(players)):
+        print('Player {}:'.format(p), players[p].__name__)
 
-for p in range(len(players)):
-    for _ in range(STARTS_PER_PLAYER):
-        game = dominoes.Game.new(starting_player=p)
-        while game.result is None:
-            game.make_move(*players[game.turn](game)[0])
+    # to keep track of how many times each team wins
+    wins = [0, 0]
 
-        if not game.result.points:
-            # tie
-            continue
+    # to keep track of how many points each team scores
+    points = [0, 0]
 
-        # compute winning team and points won
-        winning_team = game.result.player % 2
-        pts = game.result.points
-        if pts < 0:
-            winning_team = (winning_team + 1) % 2
-            pts = -pts
+    for p in range(len(players)):
+        for _ in range(STARTS_PER_PLAYER):
+            game = dominoes.Game.new(starting_player=p)
+            while game.result is None:
+                game.make_move(*players[game.turn](game)[0])
 
-        # record the result
-        wins[winning_team] += 1
-        points[winning_team] += pts
+            if not game.result.points:
+                # tie
+                continue
 
-print('Wins:', wins)
-print('Points:', points)
+            # compute winning team and points won
+            winning_team = game.result.player % 2
+            pts = game.result.points
+            if pts < 0:
+                winning_team = (winning_team + 1) % 2
+                pts = -pts
+
+            # record the result
+            wins[winning_team] += 1
+            points[winning_team] += pts
+
+    print('Wins:', wins)
+    print('Points:', points)
+
+play(team0, team1)
