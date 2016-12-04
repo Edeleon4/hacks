6f1025def54111f126d1b129c0beee282315def7
Alan Wagner <alanwagner333@gmail.com>
2016-12-04 17:19:33 -0500

    bota_gorda()

--- lib/players.py
+++ lib/players.py
@@ -4,3 +4,6 @@ def random(game):
     moves = list(game.valid_moves)
     rand.shuffle(moves)
     return moves
+
+def bota_gorda(game):
+    return sorted(game.valid_moves, key=lambda m: m[0].first + m[0].second, reverse=True)
