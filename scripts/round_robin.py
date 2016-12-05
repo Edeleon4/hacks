bf7aa7acd78108b6d61afd923848ab4f8de23ec9
Alan Wagner <alanwagner333@gmail.com>
2016-12-04 20:47:35 -0500

    moving elo function to lib

--- scripts/round_robin.py
+++ scripts/round_robin.py
@@ -1,5 +1,6 @@
 import dominoes
 import itertools
+import lib.elo
 import lib.players
 import tqdm
 
@@ -35,26 +36,6 @@ print('TEAMS:')
 for (player0, player1) in TEAMS:
     print('    ({}, {})'.format(player0.__name__, player1.__name__))
 print()
-
-def update_elo(elo0, elo1, actual_outcome0, actual_outcome1,
-               k_factor=16, magnification_interval=400, magnification_factor=10):
-    relative_elo0 = pow(magnification_factor, elo0 / magnification_interval)
-    relative_elo1 = pow(magnification_factor, elo1 / magnification_interval)
-
-    elo_normalization_constant = relative_elo0 + relative_elo1
-
-    expected_outcome_ratio0 = relative_elo0 / elo_normalization_constant
-    expected_outcome_ratio1 = relative_elo1 / elo_normalization_constant
-
-    total_score = actual_outcome0 + actual_outcome1
-
-    expected_outcome0 = total_score * expected_outcome_ratio0
-    expected_outcome1 = total_score * expected_outcome_ratio1
-
-    updated_elo0 = elo0 + k_factor * (actual_outcome0 - expected_outcome0)
-    updated_elo1 = elo1 + k_factor * (actual_outcome1 - expected_outcome1)
-
-    return updated_elo0, updated_elo1
 
 def play_game(team0, team1, starting_player):
     # set playing order
@@ -94,7 +75,7 @@ for _ in tqdm.trange(STARTS_PER_PLAYER, leave=False):
             RECORDS[(team0, team1)][1] += score1
 
             # update_elo
-            ELO[team0], ELO[team1] = update_elo(ELO[team0], ELO[team1], score0, score1)
+            ELO[team0], ELO[team1] = lib.elo.update(ELO[team0], ELO[team1], score0, score1)
 
 print('Records:')
 for ((player0, player2), (player1, player3)), record in sorted(RECORDS.items(), key=lambda pairing_record: pairing_record[1][1]):
