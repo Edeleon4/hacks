9733b66d27cd85b100e9be16c8f7e0750e41e295
Alan Wagner <alanwagner333@gmail.com>
2016-12-10 19:16:13 -0500

    moving nCk() and partitions() to utils.py

--- lib/utils.py
+++ lib/utils.py
@@ -1,4 +1,6 @@
 import contextlib
+import itertools
+import math
 import time
 
 class Timer:
@@ -10,3 +12,20 @@ def timer():
     start = time.time()
     yield t
     t.elapsed = time.time() - start
+
+def nCk(n, k):
+    return math.factorial(n) // math.factorial(n - k) // math.factorial(k)
+
+def partitions(elements, sizes):
+    try:
+        size = sizes[0]
+    except IndexError:
+        yield ()
+        raise StopIteration
+
+    remaining_sizes = sizes[1:]
+
+    for partition in itertools.combinations(elements, size):
+        remaining_elements = set(elements) - set(partition)
+        for other_partitions in partitions(remaining_elements, remaining_sizes):
+            yield (partition,) + other_partitions
--- scripts/all_possible_hands.py
+++ scripts/all_possible_hands.py
@@ -1,35 +1,16 @@
-import itertools
 import dominoes
 import lib.utils as utils
-import math
 
 FIXED_MOVES = 10
 
 print('FIXED_MOVES:', FIXED_MOVES)
-
-def nCk(n, k):
-    return math.factorial(n) // math.factorial(n - k) // math.factorial(k)
-
-def partitions(elements, sizes):
-    try:
-        size = sizes[0]
-    except IndexError:
-        yield ()
-        raise StopIteration
-
-    remaining_sizes = sizes[1:]
-
-    for partition in itertools.combinations(elements, size):
-        remaining_elements = set(elements) - set(partition)
-        for other_partitions in partitions(remaining_elements, remaining_sizes):
-            yield (partition,) + other_partitions
 
 def all_possible_hands(game):
     other_hands = tuple(h for p, h in enumerate(game.hands) if p != game.turn)
     other_dominoes = tuple(d for h in other_hands for d in h)
     other_hand_sizes = tuple(len(h) for h in other_hands)
 
-    for hands in partitions(other_dominoes, other_hand_sizes):
+    for hands in utils.partitions(other_dominoes, other_hand_sizes):
         yield tuple(dominoes.Hand(h) for h in hands)
 
 # initialize random game
@@ -41,8 +22,8 @@ for _ in range(FIXED_MOVES):
 
 # calculate expected amount of possible hands
 other_hand_lengths = [len(h) for p, h in enumerate(game.hands) if p != game.turn]
-expected_count = nCk(sum(other_hand_lengths), other_hand_lengths[0]) * \
-    nCk(other_hand_lengths[1] + other_hand_lengths[2], other_hand_lengths[1])
+expected_count = utils.nCk(sum(other_hand_lengths), other_hand_lengths[0]) * \
+    utils.nCk(other_hand_lengths[1] + other_hand_lengths[2], other_hand_lengths[1])
 
 # count all possible hands
 with utils.timer() as t:
