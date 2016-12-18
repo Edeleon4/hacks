0d2fffb0460d04e4543640c4356468e8f02d4e3a
Eduardo de Leon <edeleon4@mit.edu>
2016-12-18 16:05:30 -0500

    Fixed random new line

--- lib/players.py
+++ lib/players.py
@@ -109,8 +109,7 @@ class all_possible_hands:
 
 def monte_carlo_random(game):
     game = copy.deepcopy(game)
-    monte_carlo_weight = lambda move : -lib.search.monte_carlo_score(game,
-            move, random, 100)
+    monte_carlo_weight = lambda move : -lib.search.monte_carlo_score(game, move, random, 100)
     game.valid_moves = tuple(sorted(game.valid_moves, key=monte_carlo_weight))
 
 class compose:
